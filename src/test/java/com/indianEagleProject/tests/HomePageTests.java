a843339b4fa574f3b4aca2b3f87f8abcb4d03135
Praveen <praveen19.nandu@gmail.com>
2023-05-10 16:00:12 +0530

    add new features in home pages

--- src/test/java/com/indianEagleProject/tests/HomePageTests.java
+++ src/test/java/com/indianEagleProject/tests/HomePageTests.java
@@ -15,9 +15,27 @@ public class HomePageTests extends TestBase
 			throws InterruptedException {
 		homePage.doFlightSearch(Enum.valueOf(TripType.class, tripType), fromAirport,
 				toAirport,Enum.valueOf(CabinType.class, cabin),Enum.valueOf(AdultsCount.class, adults));
+		System.out.println("Demo purpose was wrong");
+				System.out.println("Demo purpose");
 	}
 	@Test
 	public void testTheDates() throws InterruptedException
+	{
+		homePage.testDates();
+		Thread.sleep(5000);
+	}
+	
+	
+	@Test
+	public void test() throws InterruptedException
+	{
+		
+		Thread.sleep(5000);
+	}
+	
+	
+	@Test
+	public void testTheDatess() throws InterruptedException
 	{
 		homePage.testDates();
 		Thread.sleep(5000);
