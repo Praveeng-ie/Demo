0a18534602c9b2a84634644f087e8c845be23395
Praveen <praveen19.nandu@gmail.com>
2023-05-31 16:58:44 +0530

    test

--- src/test/java/com/indianEagleProject/tests/TestBase.java
+++ src/test/java/com/indianEagleProject/tests/TestBase.java
@@ -51,12 +51,7 @@ public class TestBase {
 	@Parameters("browser")
 	@BeforeMethod
 	public void testSetup(String browser) throws FileNotFoundException, IOException, InterruptedException {
-		String useIndianUrl = System.getProperty("url");
-	    String url = "www.ieagle.com"; 
-
-	    if (useIndianUrl != null && Boolean.parseBoolean(useIndianUrl)) {
-	        url = "www.indianeagle.com"; 
-	    }
+         String url=  System.getProperty("url");
 	    driver = WebDriverUtil.createDriver(browser);
 	    Thread.sleep(5000);
 		driver.get(url);
